671b36644177a894d9ba07f47e5b8ab1039b0035
borkarfaiz <borkarfaiz@gmail.com>
2019-02-22 17:19:42 +0530

    added navbar at the top and some styling has been done

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='home'),
+    path('post/<int:pk>/', views.post_detail, name='detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
@@ -7,3 +7,12 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.all().order_by('created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    posts = get_object_or_404(Post, pk=pk)
+    print(posts)
+    # posts = get_list_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'posts': posts})
+
+
